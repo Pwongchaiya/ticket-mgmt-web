f607e9694115f90cb6b4361574fd941187bfaaa9
Pwongchaiya <Pwongchaiya1@gmail.com>
2024-10-01 13:53:30 -0700

    Update Page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client"
 
+import { ReactNode } from 'react';
 import TicketList from "./Components/Tickets/TicketList";
 import Modal from "./Components/Modal/Modal";
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/footer';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">{children}</main>
+      <Footer />
+    </div>
+  );
+};
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <Layout>
       <Modal />
       <TicketList />
-    </div>
+    </Layout>
   );
 };
 
